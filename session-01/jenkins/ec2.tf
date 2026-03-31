57c83c1286d75b219bbc03f0272efc357ab87541
Prudhvibhupathiraju <147089185+Prudhvibhupathiraju@users.noreply.github.com>
2026-03-31 07:52:25 +0000

    dahjk

--- session-01/jenkins/ec2.tf
+++ session-01/jenkins/ec2.tf
@@ -4,33 +4,29 @@ resource "aws_instance" "jenkins" {
   vpc_security_group_ids = [data.aws_security_group.allow_all.id] # this means list
   key_name = "Ayaansh-wrk-Mumbai"
   user_data = <<-EOF
-#!/bin/bash
-set -ex
+                    #!/bin/bash
+                    set -ex
 
-# Log everything
-exec > /var/log/user-data.log 2>&1
+                    # Log everything
+                    exec > /var/log/user-data.log 2>&1
 
-# Update system
-sudo yum update -y
+                    # Install minimal required packages
+                    sudo yum install -y wget curl java-17-openjdk
 
-# Install Java + wget
-sudo yum install -y java-17-openjdk wget
+                    # Add Jenkins repo
+                    sudo curl -L -o /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 
-# Add Jenkins repo
-sudo curl -o /etc/yum.repos.d/jenkins.repo \
-https://pkg.jenkins.io/redhat-stable/jenkins.repo
+                    # Import key
+                    sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 
-# Import key
-sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+                    # Install Jenkins
+                    sudo yum install -y jenkins
 
-# Install Jenkins
-sudo yum install -y jenkins
-
-# Start Jenkins
-sudo systemctl daemon-reexec
-sudo systemctl start jenkins
-sudo systemctl enable jenkins
-EOF
+                    # Start Jenkins
+                    sudo systemctl daemon-reexec
+                    sudo systemctl start jenkins
+                    sudo systemctl enable jenkins
+                    EOF
   tags = {
     Name = "Jenkins"
   }
