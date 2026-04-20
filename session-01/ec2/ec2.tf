31f52f7b6a89bde0bd733553c98db18f8ed60a96
Prudhvibhupathiraju <147089185+Prudhvibhupathiraju@users.noreply.github.com>
2026-04-20 13:56:12 +0000

    gyiahdg

--- session-01/ec2/ec2.tf
+++ session-01/ec2/ec2.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "web" {
   ami           = data.aws_ami.rhel.id
   instance_type = "t2.micro"
   vpc_security_group_ids = [data.aws_security_group.allow_all.id] # this means list
-  key_name = "Ayaansh-wrk-Mumbai"
+  key_name = "prudhvi-key-mumbai"
 
   tags = {
     Name = "HelloTerraform"
