09ada22cc679c9394066a9d741e4eb3e0736b1e1
Prudhvibhupathiraju <147089185+Prudhvibhupathiraju@users.noreply.github.com>
2026-05-26 15:27:32 +0530

    Update ec2.tf

--- session-01/ec2/ec2.tf
+++ session-01/ec2/ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "web" {
   ami           = data.aws_ami.rhel.id
-  instance_type = "t2.micro"
+  instance_type = "t3.medium"
   vpc_security_group_ids = [data.aws_security_group.allow_all.id] # this means list
   key_name = "prudhvi-key-mumbai"
 
